084377e5e6e331ea5d3265a33e9b99b37e838a2e
fernando <fernando.lima@posthaus.com.br>
2018-09-20 13:16:17 -0300

    projeto base

---
+++ src/com/lima/logica/Logica.java
@@ -0,0 +1,18 @@
+package com.lima.logica;
+
+/**
+ * 
+ * @author Fernando Lima
+ *
+ */
+public class Logica {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
